b8a130cb977899ac6594ba3c25e6c065efba082a
zansangeeth <zansangeeth17@gmail.com>
2022-04-18 01:34:49 +0530

    Initial commit

---
+++ app/src/main/java/com/zasa/namegame/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.zasa.namegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickeMe(view: View) {
+
+        tvName.text = etName.text
+    }
+}
